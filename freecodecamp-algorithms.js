46b361de014ebcaf3b183a63b10144d59355c15c
Timothy <timh1203@gmail.com>
2018-01-25 20:18:29 -0500

    Solve drop it

--- freecodecamp-algorithms.js
+++ freecodecamp-algorithms.js
@@ -551,3 +551,31 @@ function findElement(arr, func) {
 
 findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
 findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; });
+
+/////////////////////
+// Drop it (1/25/2018)
+// https://www.freecodecamp.org/challenges/drop-it
+/////////////////////
+// Drop the elements of an array (first argument), starting from the front, until the predicate (second argument) returns true.
+
+// The second argument, func, is a function you'll use to test the first elements of the array to decide if you should drop it or not.
+
+// Return the rest of the array, otherwise return an empty array.
+
+function dropElements(arr, func) {
+	let newArr = [];
+	for (let i in arr) {
+		let result = func(arr[i]);
+		
+		if (result === true) {
+			newArr = arr.slice(i);
+			break;
+		}
+	}
+	return newArr;
+}
+
+dropElements([1, 2, 3], function(n) {return n < 3; }); //[1, 2, 3]
+dropElements([1, 2, 3, 4], function(n) {return n >= 3;}) // [3,4]
+dropElements([0, 1, 0, 1], function(n) {return n === 1;}) // [1,0,1]
+dropElements([1, 2, 3, 4], function(n) {return n > 5;}) // []
