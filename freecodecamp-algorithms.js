0ecc5ea2447e00fe82cfa1fac1cf0349147e34ca
Timothy <timh1203@gmail.com>
2018-01-30 08:59:57 -0500

    Solve arguments optional

--- freecodecamp-algorithms.js
+++ freecodecamp-algorithms.js
@@ -853,3 +853,43 @@ truthCheck([{"single": ""}, {"single": "double"}], "single") // false
 truthCheck([{"single": "double"}, {"single": undefined}], "single") // false
 
 truthCheck([{"single": "double"}, {"single": NaN}], "single") // false
+
+/////////////////////
+// Arguments Optional (1/30/2018)
+// https://www.freecodecamp.org/challenges/arguments-optional
+/////////////////////
+// Create a function that sums two arguments together. If only one argument is provided, then return a function that expects one argument and returns the sum.
+
+// For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
+
+// Calling this returned function with a single argument will then return the sum:
+
+// var sumTwoAnd = addTogether(2);
+
+// sumTwoAnd(3) returns 5.
+
+// If either argument isn't a valid number, return undefined.
+function addTogether() {
+	if (typeof arguments[0] === "number") {
+		if (typeof arguments[1] === "number") {
+			return arguments[0] + arguments[1];
+		} 
+		else if (arguments[1] === undefined) {
+			let sumTwoAnd = function(y) {
+				return addTogether(2, y);
+			}
+			return sumTwoAnd;
+		}
+		else if (typeof arguments[2] !== "number") {
+			return undefined;
+		}	
+	} else {
+		return undefined;
+	}
+}
+
+addTogether(2, 3); // 5
+addTogether(2)(3); // 5
+addTogether("http://bit.ly/IqT6zt"); // undefined
+addTogether(2, "3"); // undefined
+addTogether(2)([3]) // undefined
